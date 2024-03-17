2ec52ae835697c061cbf9a6d79b186d056a3f9ba
abdulwariskhan <hellokhan.ch@gmail.com>
2024-03-17 21:36:57 +0100

    Initial Commit

---
+++ CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Khan on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
